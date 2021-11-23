1e80672ddd8c21e3a59fd029eabfdd9464b5dd73
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-11-23 17:33:12 +0900

    update: デザインにbootstrapを適応させる

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,33 +1,54 @@
 <template>
-  <div id="app">
-    <div class="form">
-      <div class="form-group">
-        <input v-model="title" placeholder="title" class="form-control" />
+  <div id="app" class="container">
+    <h4 class="mt-3 text-center">めもあぷり</h4>
+    <div class="row">
+      <div class="col-10 offset-1 col-md-6 offset-md-3">
+        <div class="form">
+          <div class="form-group">
+            <label for="title" class="form-label">項目</label>
+            <input
+              type="text"
+              v-model="title"
+              placeholder="項目"
+              class="form-control"
+            />
+          </div>
+          <div class="form-group">
+            <label for="description" class="form-label">詳細</label>
+            <input
+              type="text"
+              v-model="description"
+              placeholder="詳細"
+              class="form-control"
+            />
+          </div>
+          <button
+            class="mt-2 btn btn-secondary"
+            @click="addMemo"
+            :disabled="!buttonEnabled"
+            :title="
+              !buttonEnabled ? 'title と descriptionを入力してください' : ''
+            "
+          >
+            メモを追加
+          </button>
+        </div>
       </div>
-      <div class="form-group">
-        <input
-          v-model="description"
-          placeholder="description"
-          class="form-control"
-        />
-      </div>
-      <button
-        @click="addMemo"
-        :disabled="!buttonEnabled"
-        :title="!buttonEnabled ? 'title と descriptionを入力してください' : ''"
-      >
-        メモを追加
-      </button>
     </div>
-    <div class="flex">
-      <div v-for="memo in memos" :key="memo.id" class="card">
+
+    <div class="d-flex flex-wrap mt-4">
+      <div v-for="memo in memos" :key="memo.id" class="card col-6 col-md-4">
         <div class="card-body">
           <div class="card-title">
             {{ memo.title }}
           </div>
-          {{ memo.description }}
+          <div class="card-text">
+            {{ memo.description }}<br />
+            <button class="btn btn-secondary" v-on:click="deleteMemo(memo.id)">
+              削除
+            </button>
+          </div>
         </div>
-        <button v-on:click="deleteMemo(memo.id)">削除</button>
       </div>
     </div>
   </div>
@@ -79,45 +100,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.form {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: 32px;
-  &-group {
-    margin-bottom: 1rem;
-  }
-  &-control {
-    width: 600px;
-    min-height: 24px;
-    font-size: 1rem;
-    border: 1px solid #ced4da;
-    padding: 4px 8px;
-  }
-}
-
-button {
-  width: 200px;
-}
-
-.flex {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-}
-
-.card {
-  width: 238px;
-  border: 1px solid rgba(0, 0, 0, 0.125);
-  border-radius: 0.25rem;
-  margin: 16px;
-  &-body {
-    padding: 1.25rem;
-  }
-  &-title {
-    margin-bottom: 0.75rem;
-    font-weight: 600;
-  }
+#app {
+  max-width: 960px;
 }
 </style>
